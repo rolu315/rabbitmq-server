2736f21acc9bfe29189f448a1cf16154dace90b2
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2018-10-25 15:38:49 +0200

    rabbit_amqqueue: Deny the creation of a quorum queue if the feature flag is disabled

--- src/rabbit_quorum_queue.erl
+++ src/rabbit_quorum_queue.erl
@@ -109,7 +109,7 @@ init_state({Name, _}, QName) ->
 handle_event({ra_event, From, Evt}, QState) ->
     rabbit_fifo_client:handle_ra_event(From, Evt, QState).
 
-declare(Q) when ?is_amqqueue(Q) ->
+declare(Q) when ?amqqueue_is_quorum(Q) ->
     QName = amqqueue:get_name(Q),
     Durable = amqqueue:is_durable(Q),
     AutoDelete = amqqueue:is_auto_delete(Q),
