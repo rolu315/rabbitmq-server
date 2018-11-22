98642852f4c93b4982e72ea1d3f8a041a433b7ba
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2018-11-22 11:48:38 +0100

    rabbit_feature_flags: Add inline documentation

--- src/rabbit_feature_flags.erl
+++ src/rabbit_feature_flags.erl
@@ -14,6 +14,54 @@
 %% Copyright (c) 2018 Pivotal Software, Inc.  All rights reserved.
 %%
 
+%% @author The RabbitMQ team
+%% @copyright 2018 Pivotal Software, Inc.
+%%
+%% @doc
+%% This module offers a framework to declare capabilities a RabbitMQ node
+%% supports and therefore a way to determine if multiple RabbitMQ nodes in
+%% a cluster are compatible and can work together.
+%%
+%% == What a feature flag is ==
+%%
+%% A <strong>feature flag</strong> is a name and several properties given
+%% to a change in RabbitMQ which impacts its communication with other
+%% RabbitMQ nodes. This kind of change can be:
+%% <ul>
+%% <li>an update to an Erlang record</li>
+%% <li>a modification to a replicated Mnesia table schema</li>
+%% <li>a modification to Erlang messages exchanged between Erlang processes
+%%   which might run on remote nodes</li>
+%% </ul>
+%%
+%% A feature flag is qualified by:
+%% <ul>
+%% <li>a <strong>name</strong></li>
+%% <li>a <strong>description</strong> (optional)</li>
+%% <li>a list of other <strong>feature flags this feature flag depends on
+%%   </strong> (optional). This can be useful when the change builds up on
+%%   top of a previous change. For instance, it expands a record which was
+%%   already modified by a previous feature flag.</li>
+%% <li>a <strong>migration function</strong> (optional). If provided, this
+%%   function is called when the feature flag is enabled. It is responsible
+%%   for doing all the data conversion, if any, and confirming the feature
+%%   flag can be enabled.</li>
+%% <li>a level of stability (stable or experimental). For now, this is only
+%%   informational. But it might be used for specific purposes in the
+%%   future.</li>
+%% </ul>
+%%
+%% == How to declare a feature flag ==
+%%
+%% To define a new feature flag, you need to use the `rabbitmq_feature_flag()'
+%% module attribute:
+%%
+%% ```
+%% -rabitmq_feature_flag(FeatureFlag).
+%% '''
+%%
+%% `FeatureFlag' is a {@type feature_flag_modattr()}.
+
 -module(rabbit_feature_flags).
 
 -export([list/0,
@@ -61,25 +109,108 @@
 -define(FF_REGISTRY_LOADING_LOCK, {feature_flags_registry_loading, self()}).
 -define(FF_STATE_CHANGE_LOCK,     {feature_flags_state_change, self()}).
 
--type feature_flags() :: #{feature_name() => feature_props()}.
+-type feature_flag_modattr() :: {feature_name(),
+                                 feature_props()}.
+%% The value of a `-rabbitmq_feature_flag()' module attribute used to
+%% declare a new feature flag.
+
 -type feature_name() :: atom().
+%% The feature flag's name. It is used in many places to identify a
+%% specific feature flag. In particular, this is how an end-user (or
+%% the CLI) can enable a feature flag. This is also the only bit which
+%% is persisted so a node remember which feature flags are enabled.
+
 -type feature_props() :: #{desc => string(),
                            stability => stability(),
-                           migration_fun => migration_fun(),
-                           provided_by => atom()}.
--type stability() :: stable | experimental.
--type migration_fun() :: fun((feature_name(), feature_props(), enable)
-                             -> ok | {error, any()}).
+                           depends_on => [feature_name()],
+                           migration_fun => migration_fun_name()}.
+%% The feature flag properties.
+%%
+%% All properties are optional.
+%%
+%% The properties are:
+%% <ul>
+%% <li>`desc': a description of the feature flag</li>
+%% <li>`stability': the level of stability</li>
+%% <li>`depends_on': a list of feature flags name which must be enabled
+%%   before this one</li>
+%% <li>`migration_fun': a migration function specified by its module and
+%%   function names</li>
+%% </ul>
+%%
+%% Note that the `migration_fun' is a {@type migration_fun_name()}, not a
+%% {@type migration_fun()}. However, the function signature must
+%% conform to the {@type migration_fun()} signature. The reason is that
+%% {@link erl_syntax:abstract/1} must be able to represent the value of
+%% `migration_fun'.
 
--export_type([feature_name/0,
+-type feature_flags() :: #{feature_name() => feature_props_extended()}.
+%% The feature flags map as returned or accepted by several functions in
+%% this module. In particular, this what the {@link list/0} function
+%% returns.
+
+-type feature_props_extended() :: #{desc => string(),
+                                    stability => stability(),
+                                    migration_fun => migration_fun_name(),
+                                    depends_on => [feature_name()],
+                                    provided_by => atom()}.
+%% The feature flag properties, once expanded by this module when feature
+%% flags are discovered.
+%%
+%% The new properties compared to {@type feature_props()} are:
+%% <ul>
+%% <li>`provided_by': the name of the application providing the feature flag</li>
+%% </ul>
+
+-type stability() :: stable | experimental.
+%% The level of stability of a feature flag. Currently, only informational.
+
+-type migration_fun_name() :: {Module :: atom(), Function :: atom()}.
+%% The name of the module and function to call when changing the state of
+%% the feature flag.
+
+-type migration_fun() :: fun((feature_name(),
+                              feature_props_extended(),
+                              migration_fun_context())
+                             -> ok | {error, any()}).
+%% The migration function signature.
+%%
+%% It is called with context `enable' when a feature flag is being enabled.
+%% The function is responsible for this feature-flag-specific verification
+%% and data conversion. It returns `ok' if RabbitMQ can mark the feature
+%% flag as enabled an continue with the next one, if any. Otherwise, it
+%% returns `{error, any()}' if there is an error and the feature flag should
+%% remain disabled. The function must be idempotent: if the feature flag is
+%% already enabled on another node and the local node is running this function
+%% again because it is syncing its feature flags state, it should succeed.
+
+-type migration_fun_context() :: enable.
+
+-export_type([feature_flag_modattr/0,
+              feature_name/0,
               feature_props/0,
               feature_flags/0,
+              feature_props_extended/0,
               stability/0,
-              migration_fun/0]).
+              migration_fun_name/0,
+              migration_fun/0,
+              migration_fun_context/0]).
+
+-spec list() -> feature_flags().
+%% @doc
+%% Lists all supported feature flags.
+%%
+%% @returns A map of all supported feature flags.
 
 list() -> list(all).
 
--spec list(all | enabled | disabled) -> feature_flags().
+-spec list(Which :: all | enabled | disabled) -> feature_flags().
+%% @doc
+%% Lists all, enabled or disabled feature flags, dependening on the argument.
+%%
+%% @param Which The group of feature flags to return: `all', `enabled' or
+%% `disabled'.
+%% @returns A map of selected feature flags.
 
 list(all)      -> rabbit_ff_registry:list(all);
 list(enabled)  -> rabbit_ff_registry:list(enabled);
@@ -88,6 +219,15 @@ list(disabled) -> maps:filter(
                     list(all)).
 
 -spec list(all | enabled | disabled, stability()) -> feature_flags().
+%% @doc
+%% Lists all, enabled or disabled feature flags, dependening on the first
+%% argument, only keeping those having the specified stability.
+%%
+%% @param Which The group of feature flags to return: `all', `enabled' or
+%% `disabled'.
+%% @param Stability The level of stability used to filter the map of feature
+%% flags.
+%% @returns A map of selected feature flags.
 
 list(Which, Stability)
   when Stability =:= stable orelse Stability =:= experimental ->
@@ -95,7 +235,16 @@ list(Which, Stability)
                         Stability =:= get_stability(FeatureProps)
                 end, list(Which)).
 
--spec enable(feature_name() | [feature_name()]) -> ok | {error, any()}.
+-spec enable(feature_name() | [feature_name()]) -> ok |
+                                                   {error, Reason :: any()}.
+%% @doc
+%% Enables the specified feature flag or the specified list of feature flags.
+%%
+%% @param FeatureName The name or the list of names of feature flags to enable.
+%% @returns `ok' if the feature flags (and all the feature flags they depend
+%%   on) were successfully enabled, or `{error, Reason}' if one feature flag
+%%   could not be enabled (subsequent feature flags in the dependency tree are
+%%   left unchanged).
 
 enable(FeatureName) when is_atom(FeatureName) ->
     rabbit_log:debug("Feature flag `~s`: REQUEST TO ENABLE",
@@ -127,6 +276,13 @@ enable(FeatureNames) when is_list(FeatureNames) ->
     with_feature_flags(FeatureNames, fun enable/1).
 
 -spec enable_all() -> ok | {error, any()}.
+%% @doc
+%% Enables all supported feature flags.
+%%
+%% @returns `ok' if the feature flags were successfully enabled, or
+%%   `{error, Reason}' if one feature flag could not be enabled
+%%   (subsequent feature flags in the dependency tree are left
+%%   unchanged).
 
 enable_all() ->
     with_feature_flags(maps:keys(list(all)), fun enable/1).
@@ -146,6 +302,7 @@ disable_all() ->
 -spec with_feature_flags([feature_name()],
                          fun((feature_name()) -> ok | {error, any()})) ->
     ok | {error, any()}.
+%% @private
 
 with_feature_flags([FeatureName | Rest], Fun) ->
     case Fun(FeatureName) of
@@ -156,22 +313,42 @@ with_feature_flags([], _) ->
     ok.
 
 -spec is_supported(feature_name()) -> boolean().
+%% @doc
+%% Returns if a feature flag is supported by the entire cluster.
+%%
+%% This is the same as calling both {@link is_supported_locally/1} and
+%% {@link is_supported_remotely/1} with a logical AND.
+%%
+%% @param FeatureName The name of the feature flag the state belongs to.
 
 is_supported(FeatureName) when is_atom(FeatureName) ->
     is_supported_locally(FeatureName) andalso
     is_supported_remotely(FeatureName).
 
 -spec is_supported_locally(feature_name()) -> boolean().
+%% @doc
+%% Returns if a feature flag is supported by the local node.
+%%
+%% @param FeatureName The name of the feature flag the state belongs to.
 
 is_supported_locally(FeatureName) when is_atom(FeatureName) ->
     rabbit_ff_registry:is_supported(FeatureName).
 
 -spec is_supported_remotely(feature_name()) -> boolean().
+%% @doc
+%% Returns if a feature flag is supported by all remote nodes.
+%%
+%% @param FeatureName The name of the feature flag the state belongs to.
 
 is_supported_remotely(FeatureName) ->
     is_supported_remotely(FeatureName, ?TIMEOUT).
 
 -spec is_supported_remotely(feature_name(), timeout()) -> boolean().
+%% @doc
+%% Returns if a feature flag is supported by all remote nodes.
+%%
+%% @param FeatureName The name of the feature flag the state belongs to.
+%% @param Timeout The timeout to set when doing RPC calls to remote nodes.
 
 is_supported_remotely(FeatureName, Timeout) ->
     are_supported_remotely([FeatureName], Timeout).
